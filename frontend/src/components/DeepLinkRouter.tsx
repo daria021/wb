78818c5ce20f49e075e73b1b41418229e7b9b790
daria021 <dariaedigareva@gmail.com>
2025-06-16 13:55:41 +0300

    fix deeplink

--- frontend/src/components/DeepLinkRouter.tsx
+++ frontend/src/components/DeepLinkRouter.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import {resolveDeeplink} from "../services/api";
 import {useAuth} from "../contexts/auth";
@@ -12,20 +12,30 @@ interface Deeplink {
 
 export const DeepLinkRouter = () => {
   const navigate = useNavigate();
+  const { loading } = useAuth();
+  const [ isRedirected, setIsRedirected ]  = useState<boolean>(false);
 
   useEffect(() => {
     const followDeeplink = async () => {
+      if (loading || isRedirected) {
+          return;
+      }
       const tg = (window as any).Telegram?.WebApp;
       const param: string | undefined = tg?.initDataUnsafe?.start_param;
-      if (!param) return;
-
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      useAuth();
+      if (!param) {
+        setIsRedirected(true);
+        return;
+      }
 
       try {
         const response = await resolveDeeplink(param);
         const deeplink = response.data as Deeplink;
-        if (deeplink.url.startsWith('/')) navigate(deeplink.url, { replace: true });
+        if (deeplink.url.startsWith('/')) {
+          setIsRedirected(true);
+          navigate(deeplink.url, {
+            replace: true
+          });
+        }
       } catch (e) {
         console.error('Broken start_param', e);
       }
@@ -33,7 +43,7 @@ export const DeepLinkRouter = () => {
 
     followDeeplink();
 
-  }, [navigate]);
+  }, [loading, navigate]);
 
   return null;
 };
